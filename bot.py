ecec0481bee6543828b0f3057663edb2ddf052f7
Alexander <31483533+privetIAmAlex@users.noreply.github.com>
2018-06-01 23:57:00 +0800

    message

--- bot.py
+++ bot.py
@@ -2,5 +2,8 @@ from telebot import TeleBot
 
 bot = TeleBot("513218671:AAEa3DbXTXMfV8HH70RCx3naz9An_Z45dVQ")
 
-bot.send_message(-1001257615874, "Всем привет!\nПростите за @sayansk_zima_online")
-print("OK")
+@bot.message_handler(content_types=["text"])
+	def handle_message(message):
+		if message.chat.id == 497551952:
+			bot.send_message(-1001257615874, message.text)
+bot.polling(none_stop=True)
